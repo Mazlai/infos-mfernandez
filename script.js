c8a019fa2c1de61717be3633ed984e73b6a328b9
Mickaaa | ミキ <57178136+Micky31200@users.noreply.github.com>
2022-02-01 11:44:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function show_header() {
+	document.getElementById('h-content').style.display = 'flex';
+	document.getElementById('h-show').style.display = 'none';
+}
+function hide_header() {
+	document.getElementById('h-content').style.display = '';
+	document.getElementById('h-show').style.display = 'block';
+}
