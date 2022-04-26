76b478eb2e4359ae9dd06bb9c20adf15647774d6
Mazlai | マイ <57178136+Mazlai@users.noreply.github.com>
2022-04-26 12:21:09 +0200

    Update script.js (Form)

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function hide_header() {
 	document.getElementById('h-content').style.display = '';
 	document.getElementById('h-show').style.display = 'block';
 }
+
+function send_mail(params) {
+
+	var tempParams = {
+		from_name:document.getElementById('from-name').value,
+		user_email:document.getElementById('user-email').value,
+		subject:document.getElementById('subject').value,
+		message:document.getElementById('last-champ').value,
+	};
+
+	emailjs.send('mickygmail', 'template_ap9wgeq', tempParams)
+	.then(function(response) {
+		console.log("Success", response.status);
+	})
+}
